edcb798400c767bc9751e8789ad24b278cf5bd49
fabricio <mydata1914@gmail.com>
2023-03-05 14:41:52 +0000

    Added remote backend

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Meow! Canto de Gato</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. Este es mi primera pagina en aws.
   <!-- END -->
 
   </div>
